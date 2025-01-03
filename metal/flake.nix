6a22a9ac35b461668e9bec65d235bdc0aa89924a
jLevere <71566629+jLevere@users.noreply.github.com>
2025-01-02 21:47:46 -0500

    added nix flake for enviroment

---
+++ metal/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        venvDir = ".venv";
+        packages = with pkgs;
+          [python312]
+          ++ (with pkgs.python312Packages; [
+            pip
+            venvShellHook
+            uv
+            python312Packages.ansible-core.out
+          ]);
+      };
+    });
+  };
+}
